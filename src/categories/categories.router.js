43c80e9b4614eec30947a9b2c769f7e7e5b311f7
Garrett <seeleysurfer01@gmail.com>
2021-12-01 17:12:36 -0600

    final commit

--- src/categories/categories.router.js
+++ src/categories/categories.router.js
@@ -2,7 +2,7 @@ const router = require("express").Router({ mergeParams: true });
 const controller = require("./categories.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list).all(methodNotAllowed);
 router.route("/:categoryId").all(methodNotAllowed);
+router.route("/").get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
