6d5d9705478038c0552e2731257550f615a675fa
Garrett <seeleysurfer01@gmail.com>
2021-12-01 09:11:03 -0600

    Aggregates with knex checkpoint complete

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -24,8 +24,18 @@ async function listOutOfStockCount(req, res, next) {
   res.json({ data: await productsService.listOutOfStockCount() });
 }
 
+async function listPriceSummary(req, res, next) {
+  res.json({ data: await productsService.listPriceSummary() })
+}
+
+async function listTotalWeightByProduct(req, res) {
+  res.json({ data: await productsService.listTotalWeightByProduct() });
+}
+
 module.exports = {
   read: [asyncErrorBoundary(productExists), read],
   list: [asyncErrorBoundary(list)],
   listOutOfStockCount: asyncErrorBoundary(listOutOfStockCount),
+  listPriceSummary: asyncErrorBoundary(listPriceSummary),
+  listTotalWeightByProduct: asyncErrorBoundary(listTotalWeightByProduct),
 };
